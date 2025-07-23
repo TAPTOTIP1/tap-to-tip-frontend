dfa1a5eff79bfe89238144d0768ec5c37e5a083a
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:58:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TipForm from "./pages/TipForm";
 import Success from "./pages/Success";
 import Return from "./pages/Return";
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<TipForm />} />
         <Route path="/success" element={<Success />} />
         <Route path="/return/:connectedAccountId" element={<Return />} />
         <Route path="*" element={<div style={{ padding: "2rem" }}><h2>404 Not Found</h2></div>} />
