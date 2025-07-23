aa5d4628c89f7e20c52f591ab3cce42200689040
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:14:22 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Return from "./pages/Return";
+import Success from "./pages/Success";
+import Cancel from "./pages/Cancel";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/return/:connectedAccountId" element={<Return />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/cancel" element={<Cancel />} />
+      </Routes>
+    </Router>
+  );
+}
