8f8c1dac7ee570b663a8df61512e2a256b935a78
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:15:10 -0400

    Create Success.js

---
+++ src/pages/Success.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Success() {
+  return (
+    <div className="container">
+      <h2>Thank you for your tip!</h2>
+      <p>Your payment was successful.</p>
+    </div>
+  );
+}
