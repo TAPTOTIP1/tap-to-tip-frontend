b99acbb27493c74686a76c1df21b4d567935cf07
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:18:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Return from "./pages/Return";
-import Success from "./pages/Success";
-import Cancel from "./pages/Cancel";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Return from './pages/Return';
+import Success from './pages/Success';
+import Cancel from './pages/Cancel';
 
 export default function App() {
   return (
