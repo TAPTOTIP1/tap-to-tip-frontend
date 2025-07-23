9c078cc1c275e7a28514555493c1bf878feaac60
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:23:38 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Return from './pages/Return';
-import Success from './pages/Success';
-import Cancel from './pages/Cancel';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Success from "./pages/Success";
+import Return from "./pages/Return";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/return/:connectedAccountId" element={<Return />} />
         <Route path="/success" element={<Success />} />
-        <Route path="/cancel" element={<Cancel />} />
+        <Route path="/return/:connectedAccountId" element={<Return />} />
+        <Route path="*" element={<div style={{ padding: "2rem" }}><h2>404 Not Found</h2></div>} />
       </Routes>
     </Router>
   );
