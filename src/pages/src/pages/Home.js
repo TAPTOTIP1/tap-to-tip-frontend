ffd59b64e6008e2d889de34f91e95e78c5d81bc9
TAPTOTIP1 <tipbuisness0@gmail.com>
2025-07-23 02:10:29 -0400

    Create Home.js

---
+++ src/pages/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Welcome to Tap-to-Tip</h1>
+      <p>This will be the onboarding / tip page.</p>
+    </div>
+  );
+}
